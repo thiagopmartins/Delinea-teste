51477263fd1eda54c893335b03eb75def86c2a58
Thiago Martins <thiagopmartins@outlook.com>
2019-03-04 11:49:15 -0300

    ajustando estrutura do projeto para diferenciar a pasta de components

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,7 +2,7 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './login.components/login.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
