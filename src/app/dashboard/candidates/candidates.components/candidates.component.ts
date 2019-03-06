747e4c5dee3169fcf018a26ca23efbd2b656010c
Thiago Martins <thiagopmartins@outlook.com>
2019-03-06 16:39:31 -0300

    iniciando formulário cadastro usuario

--- src/app/dashboard/candidates/candidates.components/candidates.component.ts
+++ src/app/dashboard/candidates/candidates.components/candidates.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { Candidate } from 'src/app/models/candidate.model';
 import { DialogService } from 'src/app/providers/dialog.service';
 import { CandidateService } from 'src/app/providers/candidate.service';
-import { ToastsManager } from 'ng2-toastr/ng2-toastr';
+import { Validators, FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-candidates',
@@ -14,13 +14,27 @@ export class CandidatesComponent implements OnInit {
 
   candidates: Candidate[] = [];
   candidateSelected: Candidate;
+  basic: boolean;
+  form: FormGroup;
 
   constructor(
+    private candidateService: CandidateService,
     private dialogService: DialogService,
-    private candidateService: CandidateService
+    private fb: FormBuilder
   ) { }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      id: [],
+      name: ['', Validators.required],
+      cpf: ['', Validators.required],
+      rg: ['', Validators.required],
+      birth_date: ['', Validators.required],
+      phone: ['', Validators.required],
+      email: ['', Validators.required],
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
     this.listCandidates();
   }
 
@@ -34,8 +48,12 @@ export class CandidatesComponent implements OnInit {
     });
   }
 
+  onCreate(): void {
+    this.basic = true;
+  }
+
   onDelete(): void {
-    this.dialogService.confirm(`Deseja deletar o paciente ${this.candidateSelected.name} ?`)
+    this.dialogService.confirm(`Deseja deletar o candidato ${this.candidateSelected.name} ?`)
       .then((canDelete: boolean) => {
         if (canDelete) {
           this.candidateService.deleteCandidate(this.candidateSelected.id).subscribe(() => {
